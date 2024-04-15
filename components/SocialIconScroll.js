e0a995c455478be9ffdfb91e2b91bdebc03f151a
musamayousuf <uyousuf69@gmail.com>
2024-04-15 15:35:54 +0500

    .

--- components/SocialIconScroll.js
+++ components/SocialIconScroll.js
@@ -2,14 +2,20 @@ import React from 'react'
 
 function SocialIconScroll() {
   return (
-    <div className='max-[780px]:top-[79%] top-[70%] fixed z-[1000]'>
-        <div className="flex flex-col SidelogoIcon1 absolute p-2">
+    <div className="top-[70%] fixed z-[1000]">
+      <div className="flex flex-col SidelogoIcon1 absolute p-2">
         <div className="flex items-center justify-between">
-          <img
-            className="facebookSideicon relative ml-1"
-            src="/facebookSideicon.svg"
-            alt="none"
-          />
+          <a
+            href="https://www.facebook.com/alifnoonae"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="facebookSideicon relative ml-1"
+              src="/facebookSideicon.svg"
+              alt="Facebook"
+            />
+          </a>
           <img
             className="ml-6 LogoSideIcone1"
             src="/LogoSideIcone.svg"
@@ -17,34 +23,46 @@ function SocialIconScroll() {
           />
         </div>
       </div>
-        <div className="flex flex-col SidelogoIcon2 absolute p-2">
-          <div className="flex items-center justify-between">
+      <div className="flex flex-col SidelogoIcon2 absolute p-2">
+        <div className="flex items-center justify-between">
+          <a
+            href="https://www.instagram.com/alifnoonae"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               className="facebookSideicon relative"
               src="/InstaSideIcon.svg"
               alt="none"
             />
-            <img
-              className="ml-6 LogoSideIcone2"
-              src="/LogoSideIcone.svg"
-              alt="3"
-            />
-          </div>
+          </a>
+          <img
+            className="ml-6 LogoSideIcone2"
+            src="/LogoSideIcone.svg"
+            alt="3"
+          />
         </div>
-        <div className="flex flex-col SidelogoIcon3 absolute p-2">
-          <div className="flex items-center justify-between">
+      </div>
+      <div className="flex flex-col SidelogoIcon3 absolute p-2">
+        <div className="flex items-center justify-between">
+          <a
+            href="https://www.tiktok.com/@realestateuae?_t=8kzi3KmATAo&_r=1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               className="facebookSideicon relative"
-              src="/linkdinSideIcon.svg"
+              src="/InstaSideIcon.svg"
               alt="none"
             />
-            <img
-              className="ml-6 LogoSideIcone3"
-              src="/LogoSideIcone.svg"
-              alt="4"
-            />
-          </div>
+          </a>
+          <img
+            className="ml-6 LogoSideIcone3"
+            src="/LogoSideIcone.svg"
+            alt="4"
+          />
         </div>
+      </div>
     </div>
   )
 }
