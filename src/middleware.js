004bf562e8d3568f55f2f0e83f915c235954a40f
yousufamina <amina.yousuf@prime-logics.com>
2024-05-27 11:09:14 +0500

    added

--- src/middleware.js
+++ src/middleware.js
@@ -7,7 +7,7 @@ export function middleware() {
     // add the CORS headers to the response
     res.headers.append('Access-Control-Allow-Credentials', 'true')
     res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append('Access-Control-Allow-Methods', 'GET,OPTIONS,DELETE,PATCH,POST,PUT')
     res.headers.append(
         'Access-Control-Allow-Headers',
         '*'
