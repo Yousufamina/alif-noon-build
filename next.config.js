c5dd614bfc882d724798a6826f7235d21fd2ad9a
yousufamina <amina.yousuf@prime-logics.com>
2024-06-06 17:20:07 +0500

    added

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
                 source: "/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value : 'https://www.alifnoon.ae' }, 
+                    { key: "Access-Control-Allow-Origin", value : '*' }, 
                     { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,DELETE,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
