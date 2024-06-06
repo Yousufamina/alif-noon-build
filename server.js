9ba2c054587e2ecca0bf38df38a7062f702b3ece
yousufamina <amina.yousuf@prime-logics.com>
2024-06-06 21:55:51 +0500

    added

--- server.js
+++ server.js
@@ -10,23 +10,19 @@ const hostname = 'localhost'
 const port = process.env.PORT || 3000
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
-app.use(cors());
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
       const parsedUrl = parse(req.url, true)
       const { pathname, query } = parsedUrl
       // Enable CORS
-      app.use(cors());
       if (pathname === '/') {
-        console.log("called")
         await app.render(req, res, '/a', query)
       } else if (pathname === '/b') {
         await app.render(req, res, '/b', query)
       } else {
         await handle(req, res, parsedUrl)
       }
-      app.use(cors());
     } catch (err) {
       console.error('Error occurred handling', req.url, err)
       res.statusCode = 500 
