fdf74af8bac83f9f2cea0bc682fa8b051cb8eb8d
yousufamina <amina.yousuf@prime-logics.com>
2024-06-24 08:11:15 +0500

    added

--- next.config.js
+++ next.config.js
@@ -23,10 +23,6 @@ const nextConfig = {
               key: 'Access-Control-Allow-Headers',
               value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version, Content-Type',
             },
-            {
-              key: 'Content-Type',
-              value: 'application/json',
-            },
           ],
         },
       ];
