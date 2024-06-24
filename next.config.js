bf7a43e7fd5b8dbd8e66d088ca2a3693ac5bffa6
yousufamina <amina.yousuf@prime-logics.com>
2024-06-24 07:38:15 +0500

    added

--- next.config.js
+++ next.config.js
@@ -1,24 +1,32 @@
 /** @type {import('next').NextConfig} */
-
-const corsMiddleware = require('./cors');
-
 const nextConfig = {
     distDir: 'dist',
-    headers  : async() => {
-        return [
+    headers: async () => {
+      return [
+        {
+          source: "/:path*",
+          headers: [
             {
-                // matching all API routes
-                source: "/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value : '*' }, 
-                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,DELETE,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type,Authorization, Date, X-Api-Version" },
-                ]
+              key: "Access-Control-Allow-Credentials",
+              value: "true"
+            },
+            {
+              key: "Access-Control-Allow-Origin",
+              value: "*"
+            },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET,OPTIONS,DELETE,POST,PUT"
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version, Content-Type"
             }
-        ]
-    },
-    crossOrigin: 'anonymous',
-}
+          ]
+        }
+      ]
+    }
+  }
+  
 
 module.exports = nextConfig
