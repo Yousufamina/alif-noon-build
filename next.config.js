07c6ba530ad3344cb575492e7bca77f23de04b55
yousufamina <amina.yousuf@prime-logics.com>
2024-06-24 08:04:09 +0500

    added

--- next.config.js
+++ next.config.js
@@ -1,32 +1,37 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     distDir: 'dist',
     async headers() {
       return [
         {
-          source: "/:path*",
+          source: '/:path*',
           headers: [
             {
-              key: "Access-Control-Allow-Credentials",
-              value: "true"
+              key: 'Access-Control-Allow-Credentials',
+              value: 'true',
             },
             {
-              key: "Access-Control-Allow-Origin",
-              value: "*"
+              key: 'Access-Control-Allow-Origin',
+              value: '*',
             },
             {
-              key: "Access-Control-Allow-Methods",
-              value: "GET,OPTIONS,DELETE,POST,PUT"
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,OPTIONS,DELETE,POST,PUT',
             },
             {
-              key: "Access-Control-Allow-Headers",
-              value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version, Content-Type"
-            }
-          ]
-        }
-      ]
-    }
-  }
+              key: 'Access-Control-Allow-Headers',
+              value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version, Content-Type',
+            },
+            {
+              key: 'Content-Type',
+              value: 'application/json',
+            },
+          ],
+        },
+      ];
+    },
+  };
   
 
 module.exports = nextConfig
