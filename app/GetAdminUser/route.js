be357ec13be5a107d092d6699312d9c57729c0d9
yousufamina <amina.yousuf@prime-logics.com>
2024-06-25 02:16:17 +0500

    added

--- app/GetAdminUser/route.js
+++ app/GetAdminUser/route.js
@@ -1,27 +1,6 @@
 import { NextResponse } from "next/server";
 import AdminDataModel from "@/src/adminUserSchema";
 import { dbConnect } from "@/src/dbConnect";
-import corsMiddleware from "@/cors";
-// import Cors from 'cors';
-
-// const cors = Cors({
-//     methods: ['GET', 'HEAD', 'POST', 'OPTIONS', 'PUT' , 'DELETE'],
-//     // Replace with your domain
-//     origin: '*',
-//     credentials: true
-// })
-
-// // Helper method to wait for middleware to execute before continuing
-// function runMiddleware(req, res, fn) {
-//     return new Promise((resolve, reject) => {
-//         fn(req, res, (result) => {
-//             if (result instanceof Error) {
-//                 return reject(result)
-//             }
-//             return resolve(result)
-//         })
-//     })
-// }
 
 export async function POST(request, res) {
 
